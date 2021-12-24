a03e301a29a50cf1bcc39531cd3588fbb2149cec
Guillem Serra <guillemserralorenz@gmail.com>
2021-12-24 10:56:41 +0100

    enemies rotate to face player

--- Source/Archer/Enemies/Enemy.cpp
+++ Source/Archer/Enemies/Enemy.cpp
@@ -9,6 +9,8 @@ AEnemy::AEnemy()
 {
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationYaw = true;
 }
 
 // Called when the game starts or when spawned
