f03ebf84a38b377f2b1a2aa0b732182469cf943f
Guillem Serra <guillemserralorenz@gmail.com>
2022-02-11 19:08:32 +0100

    base class for enemy behaviour tree

---
+++ Source/Archer/AI/EnemyBehaviorTree.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "EnemyBehaviorTree.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCHER_API UEnemyBehaviorTree : public UBehaviorTree
+{
+	GENERATED_BODY()
+	
+};
