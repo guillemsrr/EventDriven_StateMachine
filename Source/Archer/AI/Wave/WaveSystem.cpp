d42596f6f495c4246a9252d84c8a1fb7e6dfc36c
Guillem Serra <guillemserralorenz@gmail.com>
2022-02-11 19:08:08 +0100

    basic wave system implemented

---
+++ Source/Archer/AI/Wave/WaveSystem.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WaveSystem.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+
+
+// Sets default values
+AWaveSystem::AWaveSystem()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AWaveSystem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	EnableInput(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+
+	InputComponent->BindAction("NextWave", IE_Pressed, this, &AWaveSystem::StartNextWave);
+}
+
+void AWaveSystem::StartNextWave()
+{
+	UWorld* World = GetWorld();
+	const FActorSpawnParameters SpawnParams;
+
+	for (AActor* SpawnLocation : SpawnLocations)
+	{
+		World->SpawnActor<AEnemy>(EnemyClass, SpawnLocation->GetActorLocation(), SpawnLocation->GetActorRotation(), SpawnParams);
+		//UAIBlueprintHelperLibrary::SpawnAIFromClass(GetWorld(), EnemyClass, nullptr, SpawnLocation->GetActorLocation(), SpawnLocation->GetActorRotation());
+	}
+}
+
+// Called every frame
+void AWaveSystem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/Archer/AI/Wave/WaveSystem.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Archer/Enemies/Enemy.h"
+
+#include "GameFramework/Actor.h"
+#include "WaveSystem.generated.h"
+
+UCLASS(Blueprintable)
+class ARCHER_API AWaveSystem : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWaveSystem();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<AActor*> SpawnLocations;
+	
+protected:
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AEnemy> EnemyClass;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void StartNextWave();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
