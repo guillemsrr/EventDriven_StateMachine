c11b003d5969e03ce92db76aee20b7a774a55cfb
Guillem Serra <guillemserralorenz@gmail.com>
2022-02-14 19:25:19 +0100

    added new debug macros

--- Source/Archer/Utilities/Debug.h
+++ Source/Archer/Utilities/Debug.h
@@ -1,3 +1,6 @@
 ﻿#pragma once
 
-#define DEBUG_LOG(Message) GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message); 
+#define DEBUG_LOG(Message) GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message);
+#define DEBUG_LOG_TICK(Message) GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Red,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message);
+#define DEBUG_LOG_NUMBER(Message, Number) GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message + FString::SanitizeFloat(Number));
+#define DEBUG_LOG_NUMBER_TICK(Message, Number) GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Red,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message + FString::SanitizeFloat(Number));
