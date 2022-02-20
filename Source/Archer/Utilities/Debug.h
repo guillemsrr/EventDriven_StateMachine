ed7c175ef06f1920a132829db487f70b9b1d6358
Guillem Serra <guillemserralorenz@gmail.com>
2022-02-20 17:00:54 +0100

    changed debug messages colors

--- Source/Archer/Utilities/Debug.h
+++ Source/Archer/Utilities/Debug.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#define DEBUG_LOG(Message) GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message);
-#define DEBUG_LOG_TICK(Message) GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Red,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message);
-#define DEBUG_LOG_NUMBER(Message, Number) GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message + FString::SanitizeFloat(Number));
-#define DEBUG_LOG_NUMBER_TICK(Message, Number) GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Red,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message + FString::SanitizeFloat(Number));
+#define DEBUG_LOG(Message) GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message);
+#define DEBUG_LOG_TICK(Message) GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Yellow,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message);
+#define DEBUG_LOG_NUMBER(Message, Number) GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Cyan,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message + FString::SanitizeFloat(Number));
+#define DEBUG_LOG_NUMBER_TICK(Message, Number) GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Cyan,  FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message + FString::SanitizeFloat(Number));
